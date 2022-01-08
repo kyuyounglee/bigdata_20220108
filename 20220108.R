f358d48d8c68cc4a27298cf96f2fa5a0d6c3eb8e
kyuyounglee <pia222.kr@gmail.com>
2022-01-08 12:48:02 +0900

    Add files via upload

---
+++ 20220108.R
@@ -0,0 +1,88 @@
+setwd("d:/")
+getwd()
+install.packages("ggplot2")
+library(ggplot2)
+ks <- read.csv("9.csv", colClasses = c(rep("factor", 6), rep("numeric", 3)))
+head(ks)
+
+P2_0 <- ggplot(data = ks,mapping = aes(x = 풍속))
+P2_1 <- P2_0 + geom_histogram(breaks=seq(0,25,0.5))
+P2_2 <- P2_1 + facet_wrap(~도시, ncol = 3)
+
+install.packages("dplyr")
+library(dplyr)
+wind_MeanSD_city = summarise(group_by(ks, 도시),
+                             Mean = mean(풍속), SD = sd(풍속))
+wind_MeanSD_city
+
+P2_3 =  P2_1 + facet_grid(도시~계절)
+wind_MeanSD_cityseason = summarise(group_by(ks, 도시,계절),
+                             Mean = mean(풍속), SD = sd(풍속))
+
+str(ks)
+# 연속형 데이터터
+ks$월 <- factor(ks$월, levels = as.character(1:12))
+#비 만 포함한 데이터프레임 작성
+ks_rain = ks %>% filter(날씨=="비")
+
+#1월 ~12월까지 비가 내일 날의 수
+str(ks_rain$월)
+ks_rain['num_month'] = as.integer(ks_rain$월)
+ks_rain = ks_rain[order(ks_rain$num_month),]
+ks_rain$월
+rownames(ks_rain) = 1:nrow(ks_rain)
+head(ks_rain,3)
+tail(ks_rain,3)
+
+
+
+p3_0 =  ggplot(data=ks_rain, mapping = aes(x=월))
+P3_1 =  p3_0 + geom_line(aes(group=1),stat = 'count')
+P3_2 =  P3_1 + geom_point(aes(group=1),stat = 'count')
+
+#1월부터 12월까지 비가 내린 날의 수를 꺾은 선 그래프로 그리기（도시별）
+(P3_3 <- P3_2 + facet_grid(. ~ 도시))
+
+#1월부터 12월까지 평균강수량의 꺾은 선 그래프 그리기
+P4_0 = ggplot(data=ks, mapping = aes(x=월,y=강수량))
+(P4_1 = P4_0 + stat_summary(aes(group = 1), fun=mean, geom = 'line'))
+(P4_2 = P4_1 + stat_summary(aes(group = 1), fun=mean, geom = 'point'))
+
+#1월부터 12월까지 평균강수량의 꺾은 선 그래프 그리기（도시별）
+(P4_3 = P4_2 + facet_grid(. ~도시))
+
+
+#2년치의 기온 데이터로 조정  2014 2015
+install.packages("tidyr")
+library(tidyr)
+
+ks_temp =  ks %>% filter(년 == "2014" | 년 == "2015") %>% 
+  select(도시,월,일,년,계절,기온) %>% 
+  spread(key=년,value = 기온, sep = "") %>% 
+  rename(기온2014 = 년2014, 기온2015=년2015)
+
+ks_temp %>% head()
+#산점도 그리기
+P5_0 = ggplot(data = ks_temp, mapping=aes(x=기온2014,y=기온2015))
+(P5_1 = P5_0 + geom_point())
+
+#산점도 그리기(도시별)
+(P5_2 = P5_1 + facet_wrap(~도시))
+
+#산점도 그리기（도시와 계절별）
+(P5_3 = P5_1 + facet_grid(도시 ~ 계절))
+
+P6_0 = ks %>% ggplot(mapping = aes(x=날씨))
+(P6_1 = P6_0+geom_bar(aes(fill = 계절),position='stack'))
+(P6_2 = P6_0+geom_bar(aes(fill = 계절),position='fill'))
+(P6_3 = P6_0+geom_bar(aes(fill = 계절),position='dodge'))
+(P6_4 = P6_0+geom_bar(aes(fill = 계절),position='identity'))
+
+#막대 색상 변경(매핑 아님)
+(P6_5 = P6_0 + geom_bar(fill='red'))
+
+#선의 색과 선의 종류 매핑
+(P6_6 = P6_0 + geom_line(aes(group = 도시,color=도시),stat="count") +
+  geom_point(aes(group = 도시,color=도시),stat="count"))
+
+#점의 색과 점의 종류 매핑
